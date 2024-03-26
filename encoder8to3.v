40897aacd390ccffd278d908e4e367f247a4a611
DeepanAnbazhagan <164902865+DeepanAnbazhagan@users.noreply.github.com>
2024-03-26 17:40:24 +0530

    Update encoder8to3.v

--- encoder8to3.v
+++ encoder8to3.v
@@ -20,11 +20,10 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module encoder8to3(a0,a1,a2,d0,d1,d2,d3,d4,d5,d6,d7);
-input d0,d1,d2,d3,d4,d5,d6,d7;
-output a0,a1,a2;
-                     //Write Verilog code for 8to3 encoder using dataflow modeling
-
-
-endmodule
-
+ module encoder8to3(a0,a1,a2,d0,d1,d2,d3,d4,d5,d6,d7);
+ input d0,d1,d2,d3,d4,d5,d6,d7;
+ output a0,a1,a2;
+ assign a0 = d1 | d3 | d5 | d7 ;
+ assign a1 = d2 | d3 | d6 | d7 ;
+ assign a2 = d4 | d5 | d6 | d7 ;
+ endmodule
